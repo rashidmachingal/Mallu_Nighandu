acafb6a949902164f15e0b55b9e5bc9a99897d97
Rashid Machingal <rashileocontact@gmail.com>
2022-08-21 18:09:22 +0530

    footer created

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['scholar.rotaractmora.org','upload.wikimedia.org'],
+  },
 }
 
 module.exports = nextConfig
