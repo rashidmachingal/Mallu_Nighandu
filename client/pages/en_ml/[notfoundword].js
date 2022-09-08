a29ff251b1e9ba37472eee61b5a0bfd17a613923
Rashid Machingal <rashileocontact@gmail.com>
2022-09-08 18:32:56 +0530

    word not fond page add word button and responsive design added

--- client/pages/en_ml/[notfoundword].js
+++ client/pages/en_ml/[notfoundword].js
@@ -91,6 +91,7 @@ const WordNotFound = () => {
       <div className={styles.notMessage}>
         <h3>ക്ഷമിക്കുക</h3>
         <p>നിങ്ങള്‍ അന്വേഷിച്ച "{notfoundword}" എന്ന പദത്തിന്റെ അര്ത്ഥം കണ്ടെത്താനായില്ല. സാധ്യമെങ്കില്‍, ദയവായി നിഘണ്ടുവില്‍ ചേര്‍ക്കുക.</p>
+        <button onClick={()=>router.push(`/add-new-word`)} >{notfoundword} നിഘണ്ടുവിൽ ചേർക്കുക </button>
       </div>
       </div>
     </div>
