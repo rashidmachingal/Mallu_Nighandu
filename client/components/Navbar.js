67c8a43e9dede1e2d835e27339e92a470779fdfd
Rashid Machingal <rashileocontact@gmail.com>
2022-09-09 14:46:22 +0530

    navbar logo h1 convert to image

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,15 +1,16 @@
-import { useRouter } from 'next/router';
 import style from "../styles/Navbar.module.css";
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
-
-  const router = useRouter()
 
   return (
     <>
     <nav className={style.navbarContainer}>
       <div className={style.navbarLogo}>
-        <h1 onClick={()=>router.push("/")} >മല്ലു നിഘണ്ടു</h1>
+        <Link href="/" >
+          <a><Image src="/mallu-nighandu-logo.png" width={340} height={73}/></a>
+        </Link>
       </div>
     </nav>
     </>
